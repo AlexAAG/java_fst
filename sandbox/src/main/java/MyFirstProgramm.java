371ecb8c9ccb2cd38f30500138789d0e2b22b8b8
AlexAAG <40862223+AlexAAG@users.noreply.github.com>
2020-08-07 12:41:29 -0700

    Добавления нескольких функций

--- sandbox/src/main/java/MyFirstProgramm.java
+++ sandbox/src/main/java/MyFirstProgramm.java
@@ -1,8 +1,43 @@
 public class MyFirstProgramm {
 
 	public static void main(String[] args) {
+		hello("world");
+		hello("user");
+		hello("alexey");
+		hello1("world111");
+		hello1("user111");
+		hello1("alexey111");
 
-		System.out.println("Hello, world!");
+		int len = 5;
+		System.out.println("ploshad kvadrata so storonoy " + len + " = " + area(len));
+
+		int men = 10;
+		System.out.println("ploshad kyba so storonoy " + men + " = " + area1(men));
+
+		int den1 = 200;
+		int den2 = 1000;
+		System.out.println("ploshad pryamoygolnika so storonami " + den1 + " i " + den2 + " = " + area(den1, den2));
+	}
+
+	public static void hello (String somebody) {
+		System.out.println("Hello, " + somebody + "world!");
+	}
+
+	public static void hello1 (String somebody) {
+		System.out.println("Hello, " + somebody + "world!");
+	}
+
+	public static int area(int l) {
+		return l * l;
+	}
+
+	public static int area1(int m) {
+		return m * m * m + 999;
+	}
+
+	public static int area(int a, int b) {
+		return a * b;
 	}
 
 }
+
